11230ed9eab01da443eb49ddf56eff7158a61264
Dalina <d.weidinger.16@ucl.ac.uk>
2018-08-28 21:09:56 +0200

    adding style

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Daily Exercise Week5 Day1" });
+  res.render("index", { title: "Want to find out the secret?" });
 });
 
 module.exports = router;
